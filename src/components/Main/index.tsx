fdfa160c31ae4cb0edbf293149aef3b624f27ce2
Luis Gustavo Macedo <luisgustavomacedo13@gmail.com>
2021-09-14 09:54:23 -0300

    MainContainer

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Main: React.FC = () => {
+  return(
+    <Container>
+      <div>
+        <h1>Mais vendidos</h1>
+        <span></span>
+      </div>
+
+    </Container>
+  );
+}
+
+export default Main;
---
+++ src/components/Main/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+  padding: 2.7rem 38rem;
+
+  span:after {
+    display: flex;
+    background-color: #E9BF33;
+    content: '';
+    height: .45rem;
+    width: 6.5rem;
+  }
+`;
